c36e9a5329a2516e43542588ada011b96eb8af3e
Janackeh Blackwell <janackeh@gmail.com>
2019-07-23 12:02:07 -0400

    added navbar, starting to style

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Navbar from "./components/navbar"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Events from "./components/events";
 import SingleEvent from "./components/event.js";
-import Navbar from "./components/navbar"
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Router>
         <Switch>
           <Route exact path="/" component={Events} />
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,11 +4,21 @@ class Navbar extends Component {
   render() {
       return (
           <div className="navbar" key = {this.props.index}>
+              <div>
           <a href="#description">ABOUT</a>
+          </div>
+          <div>
           <a href="#events">EVENTS</a>
+          </div>
+          <div>
           <h1>ATL QUEERS IN TECH</h1>
+          </div>
+          <div>
           <a href="#profile">PROFILE</a>
+          </div>
+          <div>
           <a href="#login">LOGIN</a>
+          </div>
 
          
           </div>
