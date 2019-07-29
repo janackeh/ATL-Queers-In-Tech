7256e0f00fab39636da8b51e095573879ac2e47c
Janackeh Blackwell <janackeh@gmail.com>
2019-07-28 23:26:17 -0400

    updated nav bar to show landing page and people

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,13 +4,14 @@ import { Navbar as BNavBar, Nav } from 'react-bootstrap'
 class Navbar extends Component {
     render() {
         return (
-            <BNavBar bg="dark" variant="dark">
+            <BNavBar sticky bg="dark" variant="dark">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#about">ABOUT</Nav.Link>
-                    <Nav.Link href="#admin">ADMIN</Nav.Link>
-                    <Nav.Link href="#events">EVENTS</Nav.Link>
-                    <Nav.Link href="#profile">PROFILE</Nav.Link>
-                    <Nav.Link href="#login">LOGIN</Nav.Link>
+                    <Nav.Link href="/">HOME[landing]</Nav.Link>
+                    <Nav.Link href="/about">ABOUT</Nav.Link>
+                    <Nav.Link href="/adminview">ADMIN</Nav.Link>
+                    <Nav.Link href="/home">EVENTS</Nav.Link>
+                    <Nav.Link href="/people">PEOPLE</Nav.Link>
+                    <Nav.Link href="/login">LOGIN</Nav.Link>
                 </Nav>
             </BNavBar>
         )
