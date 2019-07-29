4b6c637162b9ebda3f8acb72b561d9aeb401d8b3
Janackeh Blackwell <janackeh@gmail.com>
2019-07-28 23:24:29 -0400

    updated about page

---
+++ client/src/components/about.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    render () {
+        return (
+            <div>
+                <h1>About Us</h1>
+
+                ATL Queers in Tech was founded in 2019 to be a STEM meetup group in Atlanta GA for the LGBTQ+ community. We exist to provide a safe, inclusive and diverse space for all people within the LGBTQ+ community.
+                
+            </div>
+
+        )
+    }
+}
+
+export default About
